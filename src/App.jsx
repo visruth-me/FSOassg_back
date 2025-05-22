a3c1876ca4356a57c57a3da30425a735f64c9ad3
visruth-me <visruthme05@gmail.com>
2025-05-22 13:28:25 +0530

    1d anecdote

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,55 @@
 import { useState } from 'react'
 
-const Header = ({text}) => {
+const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
+
+const Header = ({text}) => <h1>{text}</h1>
+
+const Para = ({text, votes}) => {
   return (
     <div>
-      <h1>{text}</h1>
+      {text} <br />
+      has {votes} votes
     </div>
   )
 }
 
-const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
-
-const StatisticLine = ({value, text}) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
-  const handleGood = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-  }
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-  }
-  const handleBad = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
+  const handleNext = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
-  if(all === 0)
-    return (
-      <div>
-        <Header text = "give feedback"/>
-        <Button onClick = {handleGood} text = "good"/>
-        <Button onClick = {handleNeutral} text = "Neutral"/>
-        <Button onClick = {handleBad} text = "Bad"/>
-        <Header text = "statistics"/>
-        No feedback given
-      </div>
-    )
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const max = Math.max(...votes)
+  const index = votes.indexOf(max)
+
   return (
     <div>
-      <Header text = "give feedback"/>
-      <Button onClick = {handleGood} text = "good"/>
-      <Button onClick = {handleNeutral} text = "Neutral"/>
-      <Button onClick = {handleBad} text = "Bad"/>
-      <Header text = "statistics"/>
-      <table><tbody>
-      <StatisticLine text = "good" value = {good}/>
-      <StatisticLine text = "neutral" value = {neutral}/> 
-      <StatisticLine text = "bad" value = {bad}/>
-      <StatisticLine text = "all" value = {all}/>
-      <StatisticLine text = "average" value = {(good * 1 + bad * -1) / all}/>
-      <StatisticLine text = "positive" value = {`${good/all * 100} %`}/>
-      </tbody></table>
+      <Header text = {"Anecdote of the day"}/>
+      <Para text = {anecdotes[selected]} votes = {votes[selected]}/>
+      <Button onClick = {handleNext} text = {"Next Anecdote"}/>
+      <Button onClick = {handleVote} text = {"Vote"}/>
+      <Header text = {"Anecdote with most votes"}/>
+      <Para text = {anecdotes[index]} votes = {votes[index]}/>
     </div>
   )
 }
